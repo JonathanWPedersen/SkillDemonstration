ca34dd1d3b4e01b14db2485fea8325c80c6a752a
JonathanWPedersen <97184870+JonathanWPedersen@users.noreply.github.com>
2022-02-04 13:43:19 -0800

    test

---
+++ SkillDemonstration.java
@@ -0,0 +1,7 @@
+public class SkillDemonstration
+{
+    public static int add(int m, int n)
+    {
+        return m+n;
+    }
+}
---
+++ SkillDemonstrationTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemonstrationTest 
+{
+   @Test 
+   public void addTest()
+   {
+       assertEquals(3, SkillDemonstration.add(1,1));
+   } 
+}
